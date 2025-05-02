31c99c6b56846e208af090ac6cb71ee40100f66e
José Luis Montes <jlmreyes.21@gmail.com>
2025-05-02 14:50:31 +0200

    Modificaciones Identificador de señales

--- IdentSenales.py
+++ IdentSenales.py
@@ -18,15 +18,15 @@ class TrafficSignNet(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)                                  # [32,fil/2,col/2] -> [32,fil/4,col/4]
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)   # [32,fil/4,col/4] -> [64,fil/4,col/4]
 
-        self.fc1 = nn.Linear(64 * self.fil * self.col, 256)                                 # [64*8*8,1] -> [256,1]
+        self.fc1 = nn.Linear(64 * (self.fil // 4) * (self.col // 4), 256)                   # [64*8*8,1] -> [256,1]
         self.fc2 = nn.Linear(256, self.num_clas)                                            # [256,1] -> [5,1]
 
 
-    def forward(self, x):
+    def forward(self, x):        # Recibe la imagen y la hace atravesar la red
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x))) 
         x = F.relu(self.conv3(x))
-        x = x.view(x.size(0),-1)
+        x = x.view(x.size(0),-1)        # Equivalente a Flatten en tensorflow
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -34,20 +34,20 @@ class TrafficSignNet(nn.Module):
 
 # Dataset
 transform = transforms.Compose([
-    transforms.Resize((32, 32)),
+    transforms.Resize((32, 32)),        # Por decidir tamaño
     transforms.ToTensor(),
 ])
 
-train_data = datasets.ImageFolder('dataset/train', transform=transform)
-val_data = datasets.ImageFolder('dataset/val', transform=transform)
+train_data = datasets.ImageFolder('dataset/train', transform=transform)    # Dataset de entrenamiento (por especificar directorio)
+val_data = datasets.ImageFolder('dataset/val', transform=transform)        # dataset de validacion (por especificar directorio)
 
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_data, batch_size=32)
 
 
 # Train
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = TrafficSignNet(num_classes=5).to(device)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')        # Usa GPU si es posible
+model = TrafficSignNet(num_clas=5,fil=32,col=32).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -57,9 +57,10 @@ for epoch in range(10):
     for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
 
-        outputs = model(images)
+        outputs = model(images)  # Se llama al objeto como si fuera una funcion. El operador __call__() que hereda de nn.Module llama internamente a forward (no recomendado usar directamente model.forward())
         loss = criterion(outputs, labels)
 
+        # Actualizacion de pesos de la red
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -67,17 +68,25 @@ for epoch in range(10):
     print(f"Epoch {epoch+1} complete. Loss: {loss.item():.4f}")
 
 
-# Test
+# Validation
 model.eval()
 correct = 0
 total = 0
-with torch.no_grad():
+with torch.no_grad():                    # Se desactiva el calculo de gradientes durante la validacion
     for images, labels in val_loader:
         images, labels = images.to(device), labels.to(device)
+        
         outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
+        
+        _, predicted = torch.max(outputs.data, 1)    # Prediccion de la red
+        
         total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        correct += (predicted == labels).sum().item()    # Numero de predicciones correctas
 
+accuracy = 100 * correct / total
 print(f'Accuracy: {100 * correct / total:.2f}%')
+
+if accuracy > 70:
+    torch.save(model.state_dict(), 'traffic_sign_net.pth')
+    print("Modelo guardado")
 
