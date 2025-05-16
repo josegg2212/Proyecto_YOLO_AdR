753b567eee2dd8b0c13c916b7390cd7c7873d62c
josegg2212 <josegg2212@gmail.com>
2025-05-16 20:43:34 +0200

    main

---
+++ sign_detector_classifier.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+
+
+# Import yolo detector class
+from yolo_detection import YoloDetector
+
+# Import yolo classifier class
+#from yolo_classification import SignClassifier
+
+
+# Model weights
+DETECTOR_MODEL_PATH = "/ultralytics/yolo_share/training/first_train_119/weights/best.pt"
+#CLASSIFIER_MODEL_PATH= "sign_classifier_model.pth"
+
+
+# Class for sign detection and classification
+class SignDetectorClassifier():
+    def __init__(self):
+
+        # Load YOLO detector and classifier
+        self.detector = YoloDetector(DETECTOR_MODEL_PATH)
+        #self.classifier = SignClassifier(CLASSIFIER_MODEL_PATH)
+
+ 
+    # Process the image file
+    def process_image(self,image_path):
+
+        # Read the image
+        image = cv2.imread(image_path)
+
+        # Detect the sign
+        detected_image , bbox = self.detect_sign(image_path)
+        # Save the detected image
+        cv2.imwrite("detected_image.jpg", detected_image)
+
+        # Crop the image
+        cropped_image = self.crop_image(image , bbox)
+
+        # Read the cropped image and save it
+        #cv2.imwrite("cropped_image.jpg", cropped_image)
+        #cropped_image_path = "cropped_image.jpg"
+
+        # Classify the cropped image
+        #processed_image , pred= self.classifier.process_image(cropped_image_path)
+      
+        #return processed_image , pred
+        return cropped_image 
+
+    # Sign detection using a YOLO model 
+    def detect_sign(self, image_path):
+       
+        # Detect the sign
+        detected_image , bbox=self.detector.process_image(image_path)
+
+        return detected_image , bbox
+
+        
+    # Crop the sign
+    def crop_image(self, image, bbox):
+        
+        # Crop the image using bounding box
+        x1, y1, x2, y2 = bbox
+                      
+        cropped_image = image[y1:y2 , x1:x2]
+
+        return cropped_image
+
+    # Sign classification using a YOLO model 
+    def classify_sign(self, cropped_image_path):
+       
+        # Classify the sign
+        classified_image =self.classifier.process_image(cropped_image_path)
+        
+        return classified_image
+
+
+if __name__ == "__main__":
+    sign_detector_classifier = SignDetectorClassifier()
+
+    # Process the image
+    #processed_image , pred=sign_detector_classifier.process_image("path/to/image.jpg")
+    processed_image=sign_detector_classifier.process_image("sign2.jpeg")
+
+    # Save the processed image
+    #cv2.imwrite(f"processed_image_{pred}.jpg", processed_image)
+    cv2.imwrite(f"processed_image.jpg", processed_image)
+
+    # Display the processed image
+    cv2.imshow("Processed Image", processed_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    
--- yolo_detection_signs.py
+++ yolo_detection_signs.py
@@ -1,11 +1,29 @@
 from ultralytics import YOLO
 
-# Load pre-trained model weights
-model = YOLO('/ultralytics/yolo_share/training/first_train_signs_11/weights/best.pt')
+class YoloDetector:
+    def __init__(self, modelo_path):
 
-# Source to be analyzed
-source='https://previews.123rf.com/images/anmbph/anmbph1610/anmbph161000033/66064530-varios-diferentes-se%C3%B1ales-de-tr%C3%A1fico-de-cerca-en-una-calle-de-la-ciudad.jpg'
+        self.model = YOLO(modelo_path)
+    
+    def process_image(self, fuente, show = False, save = False):
 
-# Run interference (detection)
-results = model(source=source, show=True)
+        # Run inference (detection)
+        results = self.model(source=fuente, show=show, save=save, conf=0.5)
+
+        res=results[0]
+        
+        # Get the detected image
+        detected_img = res.plot()
+
+        # Get the bounding boxes coordinates
+        bboxes = res.boxes.xyxy.cpu().numpy().astype(int)
+
+        return detected_img , bboxes[0]
+
+
+
+
+
+
+
 
