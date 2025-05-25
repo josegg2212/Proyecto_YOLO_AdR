87f26dcb2c839b78e4df281c8a08f26a010f5be6
JoseLuis2106 <jlmreyes.21@gmail.com>
2025-05-25 18:45:18 +0200

    Limpieza códigos 5

--- sign_detector_classifier.py
+++ sign_detector_classifier.py
@@ -3,21 +3,16 @@ from yolo_detection_signs import YoloDetector       # Import yolo detector class
 from signs_classification import SignClassifier     # Import yolo classifier class
 
 
-# Model weights
-DETECTOR_MODEL_PATH = "traffic_sign_model.pt"
-CLASSIFIER_MODEL_PATH = "traffic_sign_net_8clases.pth"
-
-
 # Class for sign detection and classification
 class SignDetectorClassifier():
-    def __init__(self, num_classes = 8):
+    def __init__(self, num_classes = 8, detector_path = "traffic_sign_model.pt", classifier_path = "traffic_sign_net_8clases.pth"):
         # Load YOLO detector and classifier
-        self.detector = YoloDetector(DETECTOR_MODEL_PATH)
-        self.classifier = SignClassifier(CLASSIFIER_MODEL_PATH, num_clas=num_classes)
+        self.detector = YoloDetector(detector_path)
+        self.classifier = SignClassifier(classifier_path, num_clas = num_classes)
 
  
     # Process the image file
-    def process_image(self,image_path):
+    def process_image(self, image_path):
         # Read the image
         image = cv2.imread(image_path)
 
@@ -28,17 +23,17 @@ class SignDetectorClassifier():
         cv2.imwrite("detected_image.jpg", detected_image)
 
         # Process each bounding box
-        i=0
+        i = 0
         for bbox in bboxes:
             # Crop the image
-            cropped_image = self.crop_image(image , bbox)
+            cropped_image = self.crop_image(image, bbox)
 
             # Read the cropped image and save it
             cropped_image_path = "cropped_image.jpg"
             cv2.imwrite(cropped_image_path, cropped_image)
 
             # Classify the cropped image
-            processed_image, pred= self.classifier.process_image(cropped_image_path)
+            processed_image, pred = self.classifier.process_image(cropped_image_path)
 
             # Save the processed image
             cv2.imwrite(f"processed_image_{pred}_{i}.jpg", processed_image)
@@ -47,24 +42,24 @@ class SignDetectorClassifier():
             cv2.imshow("Processed Image", processed_image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            
-            i+=1
+
+            i += 1
 
 
     # Sign detection using a YOLO model 
     def detect_sign(self, image_path):
         # Detect the sign
-        detected_image , bbox=self.detector.process_image(image_path)
+        detected_image, bbox = self.detector.process_image(image_path)
 
-        return detected_image , bbox
+        return detected_image, bbox
 
-        
+
     # Crop the sign
     def crop_image(self, image, bbox):
         # Crop the image using bounding box
         x1, y1, x2, y2 = bbox
-                      
-        cropped_image = image[y1:y2 , x1:x2]
+
+        cropped_image = image[y1:y2, x1:x2]
 
         return cropped_image
 
@@ -72,15 +67,19 @@ class SignDetectorClassifier():
     # Sign classification using a YOLO model 
     def classify_sign(self, cropped_image_path):
         # Classify the sign
-        classified_image =self.classifier.process_image(cropped_image_path)
-        
+        classified_image = self.classifier.process_image(cropped_image_path)
+
         return classified_image
 
 
 
 if __name__ == "__main__":
+    # Models weights
+    DETECTOR_MODEL_PATH = "traffic_sign_model.pt"
+    CLASSIFIER_MODEL_PATH = "traffic_sign_net_8clases.pth"
+
     num_classes = 8
-    sign_detector_classifier = SignDetectorClassifier(num_classes=num_classes)
+    sign_detector_classifier = SignDetectorClassifier(num_classes = num_classes, detector_path = DETECTOR_MODEL_PATH, classifier_path = CLASSIFIER_MODEL_PATH)
 
     # Process the image and save it
     sign_detector_classifier.process_image("../sign3.png")
